0139c7106acfd99f137ada12d35b16145f9c3b90
MaykerHM <hydeki8@gmail.com>
2021-03-23 21:38:56 -0300

    next js local environment variables not working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,54 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+import styles from './home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      <main className={ styles.contentContainer }>
+        <section className={ styles.hero }>
+          <span> 👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for { product.amount } month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IYIfQBMIopZOEJzTZ3Dd2As', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
