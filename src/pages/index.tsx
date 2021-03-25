8fffa24e92522cd09ec364ea93cfda42ceea20a6
MaykerHM <hydeki8@gmail.com>
2021-03-24 21:59:13 -0300

    stripe checkout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -27,7 +27,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for { product.amount } month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={ product.priceId } />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -36,19 +36,21 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1IYIfQBMIopZOEJzTZ3Dd2As', {
-    expand: ['product']
-  })
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IYIfQBMIopZOEJzTZ3Dd2As')
 
   const product = {
     priceId: price.id,
-    amount: price.unit_amount / 100
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
   }
 
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
